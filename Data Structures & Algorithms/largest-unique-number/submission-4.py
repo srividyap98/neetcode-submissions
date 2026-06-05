37a7e3ad676880fb19f1567b74dcf579cff36a6e
Srividya Panchagnula <51413023+srividyap98@users.noreply.github.com>
2026-06-05 14:03:14 -0700

    Add: largest-unique-number - submission-4

---
+++ Data Structures & Algorithms/largest-unique-number/submission-4.py
@@ -0,0 +1,42 @@
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+
+        # check every element and store the largest element 
+        # largest_integer < curr 
+        # largest_int = curr 
+        # brute force?
+
+        hashmap = {}
+        largest_int = -1
+
+
+        for n in nums:
+            if n in hashmap:
+                hashmap[n] += 1
+            else: 
+                hashmap[n] = 1
+            
+        for i in hashmap:
+            if i <= largest_int or hashmap[i] != 1:
+                continue
+            largest_int = i
+        
+        return largest_int
+      
+
+
+
+
+
+"""
+        largest_int = -1
+
+
+        for i in nums:
+            if i <= largest_int or nums.count(i) > 1:
+                continue
+            largest_int = i
+        
+        return largest_int
+"""
+        
