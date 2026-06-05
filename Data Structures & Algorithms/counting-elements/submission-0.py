31b0ae76d8bfef4821eacf9baa40c241e1609f31
Srividya Panchagnula <51413023+srividyap98@users.noreply.github.com>
2026-06-05 15:08:54 -0700

    Add: counting-elements - submission-0

---
+++ Data Structures & Algorithms/counting-elements/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        count = 0 
+
+        for n in arr:
+            if (n + 1) in arr :
+                count += 1
+        return count
