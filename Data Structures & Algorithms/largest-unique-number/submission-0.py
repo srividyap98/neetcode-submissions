94b98d4831d49a8ec64fe1883cead1e5a5c80fec
Srividya Panchagnula <51413023+srividyap98@users.noreply.github.com>
2026-06-05 13:28:11 -0700

    Add: largest-unique-number - submission-0

---
+++ Data Structures & Algorithms/largest-unique-number/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+
+        # check every element and store the largest element 
+        # largest_integer < curr 
+        # largest_int = curr 
+        # brute force?
+
+        largest_int = -1
+
+
+        for i in nums:
+            if i <= largest_int or nums.count(i) > 1:
+                continue
+            largest_int = i
+        
+        return largest_int
