b24c8b54bd4c3827630618db8c010ef3b0fcfde0
Srividya Panchagnula <51413023+srividyap98@users.noreply.github.com>
2026-07-10 18:38:21 -0700

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mp = {}
+        for i in range(len(s)):
+            if s[i] in mp and mp[s[i]] != t[i]:
+                return False
+            if s[i] not in mp and t[i] in mp.values():
+                return False
+            mp[s[i]] = t[i]
+
+        return True
+
+
+
+
+
+        
