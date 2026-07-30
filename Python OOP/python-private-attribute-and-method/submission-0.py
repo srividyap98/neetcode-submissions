3bce36d43fa1d5916bbe1dea271e3660804b6810
Srividya Panchagnula <51413023+srividyap98@users.noreply.github.com>
2026-07-30 11:41:49 -0700

    Add: python-private-attribute-and-method - submission-0

---
+++ Python OOP/python-private-attribute-and-method/submission-0.py
@@ -0,0 +1,21 @@
+class PasswordManager:
+    def __init__(self, password: str):
+        self.__password = password 
+    
+    # TODO: Implement the verify_password method
+
+    def verify_password(self, input_password: str):
+        return self.__password == input_password
+        
+        '''if self.__password == password:
+            return True 
+        else: 
+            return False '''
+
+
+
+
+# Don't modify the code below this line
+my_password = PasswordManager("secret123")
+print(my_password.verify_password("secret123"))  # Should print: True
+print(my_password.verify_password("wrong"))      # Should print: False
